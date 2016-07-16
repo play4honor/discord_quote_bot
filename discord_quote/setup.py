0ecd47c6340942f7c96b15ed58f897447c84e01f
Charles Zhang <zhang.chuck@gmail.com>
2016-07-15 23:42:27 -0700

    Package skeleton

---
+++ discord_quote/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='discord_quote_bot',
+      version='0.1',
+      description='',
+      url='https://github.com/zhangchuck/discord_quote_bot',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['discord_quote_bot'],
+      zip_safe=False)
