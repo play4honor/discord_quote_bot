3a8efc8828796a29580c53c5bcaa37b1f3ae2592
synsypa <changjiang23@gmail.com>
2016-08-04 02:23:07 -0400

    adjusted output and permissions

--- discord_quote/discord_quote/discord_quote.py
+++ discord_quote/discord_quote/discord_quote.py
@@ -25,28 +25,39 @@ def quote(ctx, msg_id : str):
         msg_ = yield from bot.get_message(ctx.message.channel, msg_id)
         yield from bot.say('_' + msg_.author.name + 
                           ' [' + msg_.timestamp.strftime("%Y-%m-%d %H:%M:%S") + '] said:_ ```' +
-                           msg_.clean_content + '```')
+                           msg_.clean_content + '```' + 'via ' + ctx.message.author.name)
+        yield from bot.delete_message(ctx.message)
 
     except discord.errors.HTTPException:
         yield from bot.say("Quote not found in this channel")
 
 @bot.command(pass_context=True)  
 @asyncio.coroutine
-def misquote(ctx , user: discord.User):
+def misquote(ctx , target : discord.User):
     try:
-        yield from bot.send_message(ctx.message.author,
-                                   'What would you like to be misattributed to ' + user.name + '?')
+        if ctx.message.author.permissions_in(ctx.message.channel).administrator:
 
-        def priv(msg):
-            return msg.channel.is_private == True
+            user = target
+            #if target[1] == "@":
+            #    user = target
+            #else:
+            #    user = ctx.message.server.get_member(target)
 
-        reply = yield from bot.wait_for_message(timeout=60.0, author=ctx.message.author, check=priv)
+            yield from bot.send_message(ctx.message.author,
+                                       'What would you like to be misattributed to ' + user.name + '?')
 
-        faketime = datetime.datetime.now() - datetime.timedelta(minutes=5)
+            def priv(msg):
+                return msg.channel.is_private == True
 
-        yield from bot.say('_' + user.name + 
-                      ' [' + faketime.strftime("%Y-%m-%d %H:%M:%S") + '] said:_ ```' +
-                       reply.clean_content + '```')
+            reply = yield from bot.wait_for_message(timeout=60.0, author=ctx.message.author, check=priv)
+
+            faketime = datetime.datetime.now() - datetime.timedelta(minutes=5)
+
+            yield from bot.say('_' + user.name + 
+                          ' [' + faketime.strftime("%Y-%m-%d %H:%M:%S") + '] definitely said:_ ```' +
+                           reply.clean_content + '```')
+        else:
+            yield from bot.say("Insufficient Access")
         
     except discord.ext.commands.errors.BadArgument:
         yield from bot.say("User not found")
