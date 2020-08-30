3f747dc79fd43b82752ca09f957414a3799d77fd
Charles Zhang <zhangchuck@users.noreply.github.com>
2020-08-29 18:15:52 -0700

    Request should be deleted first, as we want to clean that regardless of whether or not the request succeeds

--- discord_quote/discord_quote/discord_quote.py
+++ discord_quote/discord_quote/discord_quote.py
@@ -108,6 +108,13 @@ async def quote(ctx, *, request:str):
     msg_target = request.split(' ')[0]
     reply = request.split(' ')[1:]
 
+    # Clean up request regardless of success
+    try:
+        await ctx.message.delete()
+        log.info(log_msg(['deleted_request', msg_id]))
+    except Exception as e:
+        log.warning(log_msg(['delete_request_failed', msg_id, e]))
+
     if '\r' in msg_target or '\n' in msg_target:
       # If weird users decide to separate the msg_id from the reply using a line return
       # clean it up.
@@ -144,14 +151,6 @@ async def quote(ctx, *, request:str):
                         msg_id,
                         reply]))
 
-
-    # Clean up request regardless of success
-    try:
-        await ctx.message.delete()
-        log.info(log_msg(['deleted_request', msg_id]))
-    except Exception as e:
-        log.warning(log_msg(['delete_request_failed', msg_id, e]))
-    
     try:
         # Retrieve the message
         msg_ = await ctx.channel.fetch_message(msg_id)
