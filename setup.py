3b804d59e10407e4160c97c18b130a84a159aba0
Charles Zhang <zhangchuck@users.noreply.github.com>
2020-09-24 01:05:43 -0700

    Edited setup.py for new folder structure

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='discord_quote_bot',
       version='0.1',
       description='',
-      url='https://github.com/play4honordiscord_quote_bot',
-      author='',
+      url='https://github.com/play4honor/discord_quote_bot',
+      author='play4honor',
       author_email='',
-      license='MIT',
-      packages=['discord_quote_bot'],
+      license='',
+      packages=['src'],
       zip_safe=False)
